aada6de0547d4aa2d00a2724456bea7972e5dd4b
Kavya Malviya <122686888+KavyaMalviya56@users.noreply.github.com>
2023-11-07 09:49:48 +0530

    Create mongo.py

---
+++ src/database/mongo.py
@@ -0,0 +1,23 @@
+import json
+from pymongo import MongoClient
+
+# Read the configuration file
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+
+db_url = config["mongodb"]["db_url"]
+db_name = config["mongodb"]["db_name"]
+collection_name = config["mongodb"]["collection_name"]
+
+class MongoDBManager:
+    def __init__(self):
+        self.client = MongoClient(db_url)
+        self.db = self.client[db_name]
+        self.collection = self.db[collection_name]
+
+    def store_link_in_mongodb(self,timestamp, image_link):
+        data = {"timestamp": timestamp, "image_path": image_path}
+        self.collection.insert_one(data)
+
+    def close_connection(self):
+        self.client.close()
