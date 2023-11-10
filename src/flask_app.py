25b7ba109e9c3838ed84ab8fb511113de81fdfd4
Anupam Maurya <anupammaurya6767@gmail.com>
2023-11-11 00:58:26 +0530

    Create flask_app.py

---
+++ src/flask_app.py
@@ -0,0 +1,27 @@
+from flask import Flask, jsonify
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+# Set your MongoDB URL
+app.config['MONGO_URI'] = 'mongodb+srv://noobkoda:noobkoda2004@expresso.7tk1f8j.mongodb.net/rakshak'
+mongo = PyMongo(app)
+
+@app.route('/getData', methods=['GET'])
+def get_data():
+    try:
+        # Replace 'previous' with your actual collection name
+        posts = mongo.db.previous.find({})
+        data = []
+        for post in posts:
+            data.append({
+                'timestamp': post['timestamp'],
+                'link': post['link']
+            })
+        return jsonify({'data': data})
+    except Exception as e:
+        print(str(e))
+        return jsonify({'error': 'Internal Server Error'}), 500
+
+if __name__ == '__main__':
+    app.run(port=3000)
